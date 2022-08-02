3668bd008e64d4da89a237a657ff8c701b35e053
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-08-02 21:52:34 +0300

    feat: create header component

--- starWars/src/app/app.module.ts
+++ starWars/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { WelcomeComponent } from './pages/welcome-page/welcome/welcome.component';
+import { CharactersComponent } from './pages/characters/characters/characters.component';
+import { FilmsComponent } from './pages/films/films/films.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    CharactersComponent,
+    FilmsComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
